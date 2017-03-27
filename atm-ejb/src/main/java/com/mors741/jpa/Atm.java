401a488b9aa6f365aee08ced53ee27edd70fc730
mors741 <mors741@yandex.ru>
2017-03-27 06:59:42 +0300

    CheckAtmService

--- atm-ejb/src/main/java/com/mors741/jpa/Atm.java
+++ atm-ejb/src/main/java/com/mors741/jpa/Atm.java
@@ -1,20 +1,33 @@
 package com.mors741.jpa;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
 public class Atm {
+
     @Id
     @GeneratedValue
+    @Getter
+    @Setter
     private int id;
 
     @Column
+    @Getter
+    @Setter
     private String currency;
 
     @Column
+    @Getter
+    @Setter
     private int amount;
 
 
---
+++ atm-ejb/src/main/java/com/mors741/ws/CheckAtm.java
@@ -0,0 +1,23 @@
+package com.mors741.ws;
+
+import com.mors741.jpa.Atm;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+@WebService
+public class CheckAtm {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @WebMethod
+    public String availableCash(int id) {
+        Atm atm = entityManager.find(Atm.class, id);
+        return atm.getAmount() + " " + atm.getCurrency();
+    }
+}
